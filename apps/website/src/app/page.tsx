dd2836cff64b7201ae64fe0d3949c9d92d81f350
pawelblaszczyk5 <internal@pawelblaszczyk.dev>
2024-07-31 22:01:00 +0200

    Next+effect wip

--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -1,7 +1,18 @@
-import { x } from "#src/lib/test.ts";
+import { Effect } from "effect";
 
-const HomeRoute = () => {
-	return <h1>Hello world {x}</h1>;
-};
+import { defineEffectComponent, ExampleService, noStore } from "#src/lib/effect.ts";
+
+const HomeRoute = defineEffectComponent(({ searchParams }: { searchParams: Record<string, any> }) => {
+	return Effect.gen(function* () {
+		const exampleService = yield* ExampleService;
+		const greeting = yield* exampleService.greet();
+
+		yield* Effect.log(searchParams);
+
+		yield* noStore;
+
+		return <h1>{greeting}</h1>;
+	});
+});
 
 export default HomeRoute;
---
+++ apps/website/src/lib/effect.ts
@@ -0,0 +1,70 @@
+import type { ReactNode } from "react";
+
+import { Cause, Context, Data, Effect, Exit, Layer, Logger, ManagedRuntime, Option } from "effect";
+import { unstable_noStore } from "next/cache";
+import { unstable_rethrow } from "next/navigation";
+
+const makeExampleServiceLive = () => {
+	return {
+		greet: (name?: string) => {
+			return Effect.gen(function* () {
+				if (name) return `Hello ${name}!`;
+
+				yield* Effect.log("Anonymous user visited!");
+
+				return "Hello stranger!";
+			});
+		},
+	};
+};
+
+// eslint-disable-next-line @typescript-eslint/consistent-type-definitions -- prevent intellisense expanding
+export interface ExampleService extends ReturnType<typeof makeExampleServiceLive> {}
+
+export const ExampleService = Context.GenericTag<ExampleService>("@repo/website#ExampleService");
+
+export const ExampleServiceLive = Layer.succeed(ExampleService, makeExampleServiceLive());
+
+const AppLayer = Layer.mergeAll(Logger.pretty, ExampleServiceLive);
+
+const runtime = ManagedRuntime.make(AppLayer);
+
+type AppEnvironment = Layer.Layer.Success<typeof AppLayer>;
+
+class NextInternalError extends Data.TaggedError("NextInternalError")<{ cause: unknown }> {}
+
+export const noStore = Effect.try({
+	catch: (cause) => {
+		return new NextInternalError({ cause });
+	},
+	try: () => {
+		unstable_noStore();
+	},
+});
+
+export const defineEffectComponent = <
+	Props extends Readonly<Record<string, any>> | undefined = undefined,
+	Return extends ReactNode = ReactNode,
+>(
+	component: (props: Props) => Effect.Effect<Return, NextInternalError, AppEnvironment>,
+) => {
+	return async (props: Props) => {
+		const program = component(props);
+		const result = await runtime.runPromiseExit(program);
+
+		if (Exit.isSuccess(result)) return result.value;
+
+		return result.cause.pipe(
+			Cause.failureOption,
+			Option.match({
+				onNone: () => {
+					throw new Error("Unexpected error");
+				},
+				onSome: (failure) => {
+					unstable_rethrow(failure.cause);
+					throw new Error("Unexpected error");
+				},
+			}),
+		);
+	};
+};
